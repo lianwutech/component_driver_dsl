1e1673219a94ba55e0a81b141607cfa8a2ffd349
Merlin Ran <merlinran@gmail.com>
2014-07-18 14:37:48 +0800

    show driver metadata for python example

--- invoke.py
+++ invoke.py
@@ -6,7 +6,23 @@ ctxt = PyV8.JSContext()
 ctxt.enter()
 ctxt.eval(driver)
 ctxt.eval(example)
-result = ctxt.eval("var result = driver.process_data(\"A4B83290DE\");");
+ctxt.eval("var meta_data = driver.getResult()")
+
+print "meta data:"
+print ctxt.locals.meta_data.fields.name
+print ctxt.locals.meta_data.fields.version
+
+print "errors:"
+for error in ctxt.locals.meta_data.errors:
+    print error
+
+print "actions:"
+print ctxt.locals.meta_data.actions
+
+print "states:"
+print ctxt.locals.meta_data.states
+
+result = ctxt.eval("var result = driver.process_data(\"A4B83290DE\");")
 print "raw = " + ctxt.locals.result.data.raw
 print "direction = " + ctxt.locals.result.data.direction
 print "speed = %f" % ctxt.locals.result.data.speed
